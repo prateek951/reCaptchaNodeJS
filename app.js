4b073f72c96dbf378242a308f3262a875d035823
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2017-10-05 13:48:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+
+const port = process.env.PORT || 1337;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/subscribe', (req, res) => {
+  if(req.body.captcha === undefined || req.body.captcha === '' || req.body.captcha === null)
+  {
+    return res.json({"success": false, "msg":"Please select captcha"});
+  }
+
+  // Secret Key
+  const secretKey = '6LdpvDEUAAAAAHszsgB_nnal29BIKDsxwAqEbZzU';
+
+  // Verify URL
+  const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}&remoteip=${req.connection.remoteAddress}`;
+
+  // Make Request To VerifyURL
+  request(verifyUrl, (err, response, body) => {
+    body = JSON.parse(body);
+    console.log(body);
+
+    // If Not Successful
+    if(body.success !== undefined && !body.success){
+      return res.json({"success": false, "msg":"Failed captcha verification"});
+    }
+
+    //If Successful
+    return res.json({"success": true, "msg":"Captcha passed"});
+  });
+});
+
+app.listen(port, () => console.log(`Server started on port: ${port}`));
